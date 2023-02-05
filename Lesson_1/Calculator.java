cebeab5cb77dbd6b3802b40a849482c561980bfb
Gulnaz <ggf.faery@gmail.com>
2023-02-05 22:32:01 +0300

    Итоговые ДЗ для Lesson_1: Калькулятор v.2

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -3,7 +3,7 @@ public class Calculator {
     public static void main(String[] args) {
         int a = 9;
         int b = 5;
-        char sign = '-';
+        char sign = '%';
         int result = 1;
         if (sign == '+') {
             result = a + b;
@@ -18,7 +18,7 @@ public class Calculator {
                 result *= a; 
             }
         } else if (sign == '%') {
-            result = a - b * (a / b);
+            result = a % b;
         }
         System.out.println(a + " " + sign + " " + b + " = " + result);
     }

21d27f76994a70cd2025f52e1e14529f49108150
Gulnaz <ggf.faery@gmail.com>
2023-02-05 22:39:09 +0300

    Итоговые ДЗ для Lesson_1: Игра v.2

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,16 +1,17 @@
 import java.util.Random;
+
 public class MyFirstGame {
     
     public static void main(String[] args) {
-        int totalNum = 1 + (int) (Math.random() * 100);
-        int userNum = 1 + (int) (Math.random() * 100);
-        while (userNum != totalNum) {
-            if (userNum < totalNum) {
-                System.out.println("число " + userNum + " меньше того, что загадал компьютер");
-                userNum += 5;
+        int secretNum = 1 + (int) (Math.random() * 100);
+        int playerNum = 1 + (int) (Math.random() * 100);
+        while (playerNum != secretNum) {
+            if (playerNum < secretNum) {
+                System.out.println("число " + playerNum + " меньше того, что загадал компьютер");
+                playerNum += 5;
             } else {
-                System.out.println("число " + userNum + " больше того, что загадал компьютер");
-                userNum -= 1;
+                System.out.println("число " + playerNum + " больше того, что загадал компьютер");
+                playerNum -= 1;
             }
         }
         System.out.println("Вы победили!");

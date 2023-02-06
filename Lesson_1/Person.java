aca65bd01604f0e6bb1279311310642e7056fc71
Gulnaz <ggf.faery@gmail.com>
2023-02-06 10:27:12 +0300

    ДЗ class Person v.1

---
+++ Lesson_1/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+    String sex = "male";
+    String name = "Bob";
+    double height = 1.80d;
+    double weight = 73.5d;
+    int age = 32;
+
+    void go() {
+        System.out.println("Bob is going");
+    }
+
+    void sit() {
+        System.out.println("Bob is sitting");
+    }
+
+    void run() {
+        System.out.println("Bob is running");
+    }
+
+    String say() {
+        return "Hello";
+    }
+
+    void learnJava() {
+        System.out.println("Bob is learning Java");
+    }
+
+}

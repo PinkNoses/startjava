02908fd55d96e9b9e74dbc301b06d870d04e7727
Gulnaz <ggf.faery@gmail.com>
2023-02-07 13:52:44 +0300

    ДЗ class Wolf v.1

---
+++ Lesson_2/Wolf.java
@@ -0,0 +1,28 @@
+public class Wolf {
+
+	String gender;
+	String wolfName;
+	String colour;
+	double  weight;
+	int age;
+
+	void go() {
+		System.out.println("Идет");
+	}
+
+	void sit() {
+		System.out.println("Сидит");
+	}
+
+	void run() {
+		System.out.println("Бежит");
+	}
+
+	void howl() {
+		System.out.println("Воет");
+	}
+
+	void hunt() {
+		System.out.println("Охотится");
+	}
+}
---
+++ Lesson_2/WolfTest.java
@@ -0,0 +1,21 @@
+public class WolfTest {
+
+	public static void main (String[] args) {
+		Wolf wolf = new Wolf();
+		wolf.gender = "male";
+		wolf.wolfName = "Jacob";
+		wolf.colour = "brown";
+		wolf.weight = 80.0;
+		wolf.age = 33;
+		System.out.println("gender: " + wolf.gender);
+		System.out.println("wolfName: " + wolf.wolfName);
+		System.out.println("colour: " + wolf.colour);
+		System.out.println("weight: " + wolf.weight);
+		System.out.println("age: " + wolf.age);
+		wolf.go();
+		wolf.sit();
+		wolf.run();
+		wolf.hunt();
+		wolf.howl();
+	}
+}

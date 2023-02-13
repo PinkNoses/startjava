ae506c99317f10c775482a7d6cf74db3db7c50a7
Gulnaz <ggf.faery@gmail.com>
2023-02-13 11:47:31 +0300

    Итоговое ДЗ Угадай число v.2

--- Lesson_2/GuessNumber.java
+++ Lesson_2/GuessNumber.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class GuessNumber {
 
     private int secretNum;
+    private Player player1;
+    private Player player2;
+
+    public GuessNumber(Player player1, Player player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
 
     public int getSecretNum() {
         return secretNum;
@@ -10,13 +19,32 @@ public class GuessNumber {
         this.secretNum = secretNum;
     }
 
-    public void compareNum(int playerNum, String playerName) {
-        if (playerNum == secretNum) {
-            System.out.println(playerName + ", Вы победили!");
-        } else if (playerNum > secretNum) {
-            System.out.println("число " + playerNum + " больше того, что загадал компьютер");
-        } else if (playerNum < secretNum) {
-            System.out.println("число " + playerNum + " меньше того, что загадал компьютер");
+    private void compareNum(Player player) {
+        if (player.getNumber() == secretNum) {
+            System.out.println(player.getName() + ", Вы победили!");
+        } else if (player.getNumber() > secretNum) {
+            System.out.println("число " + player.getNumber() + " больше того, что загадал компьютер");
+        } else if (player.getNumber() < secretNum) {
+            System.out.println("число " + player.getNumber() + " меньше того, что загадал компьютер");
+        }
+    }
+
+    public void startGame() {
+        Scanner sc = new Scanner(System.in);
+        while (player1.getNumber() != secretNum && player2.getNumber() != secretNum) {
+            System.out.println(player1.getName() + ", введите предполагаемое число:");
+            player1.setNumber(sc.nextInt());
+            compareNum(player1);
+            if (player1.getNumber() == secretNum) {
+                break;
+            } else {
+                System.out.println(player2.getName() + ", введите предполагаемое число:");
+                player2.setNumber(sc.nextInt());
+                compareNum(player2);
+            }
         }
     }
 }
+
+
+
--- Lesson_2/Player.java
+++ Lesson_2/Player.java
@@ -3,16 +3,12 @@ public class Player {
     private String name;
     private int number;
 
-    public Player (String name) {
+    public Player(String name) {
         this.name = name;
     }
 
     public String getName() {
         return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
     }
 
     public int getNumber() {

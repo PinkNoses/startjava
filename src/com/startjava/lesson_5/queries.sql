d988f705fc13f0bbb3402c4c8a1748be00398a2f
Gulnaz <ggf.faery@gmail.com>
2023-03-29 11:53:01 +0300

    ДЗ lesson_5 v.3

--- src/com/startjava/lesson_5/queries.sql
+++ src/com/startjava/lesson_5/queries.sql
@@ -1,49 +1,49 @@
-\echo 'Output TABLE Jaegers';
+\echo 'Output TABLE Jaegers':
 SELECT * FROM Jaegers;
 
-\echo 'Output only undestroyed Jaegers';
+\echo 'Output only undestroyed Jaegers':
 SELECT * 
   FROM Jaegers 
  WHERE status NOT IN ('destroyed');
 
-\echo 'Output Jaegers of Mark-1 and Mark-4';
+\echo 'Output Jaegers of Mark-1 and Mark-4':
 SELECT * 
   FROM Jaegers
  WHERE mark IN ('Mark-1', 'Mark-4');
 
-\echo 'Output all Jaegers except Mark-1 and Mark-4';
+\echo 'Output all Jaegers except Mark-1 and Mark-4':
 SELECT *
   FROM Jaegers
  WHERE mark NOT IN ('Mark-1', 'Mark-4');
 
-\echo 'Sort Table descending order by column mark';
+\echo 'Sort Table descending order by column mark':
 SELECT *
   FROM Jaegers
  ORDER BY mark DESC;
 
-\echo 'Output the oldest robot';
+\echo 'Output the oldest robot':
 SELECT *
   FROM Jaegers
  WHERE launch = (SELECT MIN(launch) 
                    FROM Jaegers);
 
-\echo 'Output Jaegers that destroyed the most kaiju';
+\echo 'Output Jaegers that destroyed the most kaiju':
 SELECT *
   FROM Jaegers
  WHERE kaiju_kill = (SELECT MAX(kaiju_kill)
                        FROM Jaegers);
 
-\echo 'Output the average weight of Jaegers';
+\echo 'Output the average weight of Jaegers':
 SELECT
    AVG(weight)
   FROM Jaegers;
 
-\echo 'Increase +1 of kaiju for Jaegers that is undestroyed';
+\echo 'Increase +1 of kaiju for Jaegers that is undestroyed':
 UPDATE Jaegers
    SET kaiju_kill = kaiju_kill + 1
  WHERE status NOT IN ('destroyed');
 
-\echo 'Delete destroyed Jaegers';
+\echo 'Delete destroyed Jaegers':
 DELETE
   FROM Jaegers
  WHERE status = 'destroyed';

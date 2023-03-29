386c587e8f35b2354cdf88e507dbbb080d0a8337
Gulnaz <ggf.faery@gmail.com>
2023-03-29 12:55:48 +0300

    ДЗ lesson_5 v.4

--- src/com/startjava/lesson_5/create_db.sql
+++ src/com/startjava/lesson_5/create_db.sql
@@ -5,17 +5,16 @@ CREATE DATABASE jaegers;
 \c jaegers;
 
 CREATE TABLE jaegers (
-    id          SERIAL PRIMARY KEY, 
-    model_name  TEXT, 
-    mark        TEXT, 
-    height      FLOAT8, 
-    weight      FLOAT8, 
-    status      CHAR(11),
-    origin      TEXT,
-    launch      DATE,
-    kaiju_kill  INT
+    id         SERIAL PRIMARY KEY, 
+    model_name TEXT, 
+    mark       TEXT, 
+    height     FLOAT8, 
+    weight     FLOAT8, 
+    status     CHAR(11),
+    origin     TEXT,
+    launch     DATE,
+    kaiju_kill INT
 );
 
 \ir init_db.sql;
-
 \ir queries.sql;
--- src/com/startjava/lesson_5/init_db.sql
+++ src/com/startjava/lesson_5/init_db.sql
@@ -1,11 +1,11 @@
 INSERT INTO Jaegers (model_name, mark, height, weight, status, origin, launch, kaiju_kill) 
-VALUES ('Brawler Yukon',   'Mark-1',    '77.72',    '7.775',    'destroyed',    'USA',         '2015-01-30',   '2'),
-       ('Horizon Brave',   'Mark-1',    '72.54',    '7.89',     'destroyed',    'China',       '015-12-22',    '2'),
-       ('Tacit Ronin',     'Mark-1',    '74.37',    '7.45',     'undestroyed',  'Japan',       '2015-12-06',   '3'),
-       ('Romeo Blue',      'Mark-1',    '77.72',    '7.775',    'destroyed',    'USA',         '2015-12-15',   '2'),
-       ('Solar Prophet',   'Mark-2',    '73.1',     '4.05',     'destroyed',    'Peru',        '2016-03-30',   '5'),
-       ('Eden Assassin',   'Mark-2',    '76.54',    '3.63',     'undestroyed',  'Russia',      '2016-03-16',   '20'),
-       ('Gipsy Danger',    'Mark-3',    '79.25',    '1.98',     'destroyed',    'USA',         '2017-07-10',   '9'),
-       ('Crimson Typhoon', 'Mark-4',    '76.2',     '1.722',    'destroyed',    'China',       '2018-08-22',   '7'),
-       ('Nova Hyperion',   'Mark-4',    '79.59',    '2.3',      'undestroyed',  'South Korea', '2018-05-31',   '1'),
-       ('Striker Eureka',  'Mark-5',    '76.2',     '1.85',     'destroyed',    'Australia',   '2019-11-02',   '11');
+VALUES ('Brawler Yukon',   'Mark-1', '77.72', '7.775', 'destroyed',   'USA',         '2015-01-30', '2'),
+       ('Horizon Brave',   'Mark-1', '72.54', '7.89',  'destroyed',   'China',       '015-12-22',  '2'),
+       ('Tacit Ronin',     'Mark-1', '74.37', '7.45',  'undestroyed', 'Japan',       '2015-12-06', '3'),
+       ('Romeo Blue',      'Mark-1', '77.72', '7.775', 'destroyed',   'USA',         '2015-12-15', '2'),
+       ('Solar Prophet',   'Mark-2', '73.1',  '4.05',  'destroyed',   'Peru',        '2016-03-30', '5'),
+       ('Eden Assassin',   'Mark-2', '76.54', '3.63',  'undestroyed', 'Russia',      '2016-03-16', '20'),
+       ('Gipsy Danger',    'Mark-3', '79.25', '1.98',  'destroyed',   'USA',         '2017-07-10', '9'),
+       ('Crimson Typhoon', 'Mark-4', '76.2',  '1.722', 'destroyed',   'China',       '2018-08-22', '7'),
+       ('Nova Hyperion',   'Mark-4', '79.59', '2.3',   'undestroyed', 'South Korea', '2018-05-31', '1'),
+       ('Striker Eureka',  'Mark-5', '76.2',  '1.85',  'destroyed',   'Australia',   '2019-11-02', '11');
